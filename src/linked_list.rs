37c235b25c45aba25f5c9ef0a30b104a3a691b1e
Vincent Herlemont <vincentherl@leszeros.com>
2021-01-20 16:22:35 +0000

    new version of linked_list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,81 +1,50 @@
 use std::option::Option::Some;
-use std::iter::FromIterator;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::convert::{TryFrom, TryInto};
+
+type RefNode<T> = Rc<RefCell<Node<T>>>;
 
 #[derive(Debug)]
 struct Node<T> {
     data: T,
-    next: Box<Option<Node<T>>>
+    next: Option<RefNode<T>>
 }
 
 impl<T> Node<T> {
-    fn new(data:T,ref_node: Option<Node<T>>) -> Node<T> {
+    fn new(data:T, node: Option<Node<T>>) -> Node<T> {
         Node {
             data,
-            next: Box::new(ref_node)
-        }
-    }
-
-    fn iter(&self) -> NodeItr<'_,T> {
-        NodeItr {
-            node: Some(self)
+            next: node.map(|node| Rc::new(RefCell::new(node)))
         }
     }
 }
 
-impl<T> IntoIterator for Node<T> {
-    type Item = T;
-    type IntoIter = NodeIntoIter<T>;
+impl<T> TryFrom<RefNode<T>> for Node<T> {
+    type Error = &'static str;
 
-    fn into_iter(self) -> Self::IntoIter {
-        NodeIntoIter {
-            node: Some(self)
-        }
-    }
-}
-
-impl<A> FromIterator<A> for Node<A> {
-    fn from_iter<T: IntoIterator<Item=A>>(iter: T) -> Self {
-        let v: Vec<_> = iter.into_iter().collect();
-        let mut into_iter_v = v.into_iter().rev();
-        let head = into_iter_v.next().expect("collection into Node must have at lest one element");
-        let mut head = Node::new(head, None);
-        for el in into_iter_v {
-            head = Node::new(el,Some(head));
-        }
-        head
-    }
-}
-
-struct NodeIntoIter<T> {
-    node: Option<Node<T>>
-}
-
-impl<T> Iterator for NodeIntoIter<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(mut node) = self.node.take() {
-            self.node = node.next.take();
-            Some(node.data)
+    fn try_from(node: RefNode<T>) -> Result<Self, Self::Error> {
+        if let Ok(node) = Rc::try_unwrap(node) {
+            Ok(node.into_inner())
         } else {
-            None
+            Err("can not retrieve Node from RefNode")
         }
     }
 }
 
-struct NodeItr<'n,T> {
-    node: Option<&'n Node<T>>
+struct NodeItr<T> {
+    current: Option<RefNode<T>>
 }
 
-impl<'n,T> Iterator for NodeItr<'n,T> {
-    type Item = &'n Node<T>;
+impl<T> Iterator for NodeItr<T> {
+    type Item = RefNode<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(node) = self.node {
-            let out = self.node;
-            self.node = (&*node.next).into();
-            // self.node = node.next.as_ref().into(); // &*... for Option data is equivalent to as_ref() method.
-            out
+        if let Some(current) = self.current.take() {
+            if let Some(next) = &current.borrow().next {
+                self.current = Option::from(Rc::clone(next));
+            }
+            Some(Rc::clone(&current))
         } else {
             None
         }
@@ -84,7 +53,7 @@ impl<'n,T> Iterator for NodeItr<'n,T> {
 
 #[derive(Debug)]
 struct LinkedList<T> {
-    head: Option<Node<T>>
+    head: Option<RefNode<T>>
 }
 
 impl<T> LinkedList<T> {
@@ -94,43 +63,84 @@ impl<T> LinkedList<T> {
         }
     }
 
-    fn insert_first(&mut self,data: T) {
-        self.head = Some(Node::new(data,self.head.take()));
-    }
-
-    fn size(&self) -> usize {
-        if let Some(node) = &self.head {
-            node.iter().count()
-        } else {
-            0
+    fn iter(&self) -> NodeItr<T> {
+        NodeItr {
+            current: self.head.as_ref().map(|node| Rc::clone(&node))
         }
     }
 
-    fn get_first(&self) -> Option<&Node<T>> {
-        self.head.as_ref()
+    fn insert_first(&mut self,data: T) {
+        if let Some(next) = self.head.take() {
+            let next: Node<_> = next.try_into().unwrap();
+            let node = Node::new(data, Some(next));
+            self.head = Some(Rc::new(RefCell::new(node)));
+        } else {
+            let node = Node::new(data,None);
+            self.head = Some(Rc::new(RefCell::new(node)));
+        }
     }
 
-    fn get_last(&self) -> Option<&Node<T>> {
-        self.head.as_ref().and_then(|node| {
-            node.iter().last()
-        })
+    fn size(&self) -> usize {
+        self.iter().count()
+    }
+
+    fn get_first(&self) -> Option<RefNode<T>> {
+        self.iter().next()
+    }
+
+    fn get_last(&self) -> Option<RefNode<T>> {
+        self.iter().last()
     }
 
     fn remove_first(&mut self) {
-        self.head = self.head.as_mut().and_then(|node| {
-            node.next.take()
-        });
+        if self.size() == 1 {
+            self.head = None;
+        }
+        let mut iter = self.iter().skip(1);
+        if let Some(node) = iter.next() {
+            self.head = Some(Rc::clone(&node))
+        }
     }
 
     fn remove_last(&mut self) {
-        if let Some(node) = self.head.take() {
-            let mut v: Vec<_> = node.into_iter().collect();
-            v.pop();
-            if v.is_empty() {
-                self.head = None;
-            } else {
-                self.head = Some(v.into_iter().collect())
-            }
+        let size = self.size();
+        if size >= 2 {
+            let mut iter = self.iter();
+            let before_last_node = iter.nth(size-2).unwrap();
+            let mut node = before_last_node.borrow_mut();
+            node.next = None;
+        } else {
+            self.head = None;
+        }
+    }
+
+    fn insert_last(&mut self, data: T) {
+        if let Some(last) = self.get_last() {
+            let mut last = last.borrow_mut();
+            let new_node = Rc::new(RefCell::new(Node::new(data,None)));
+            last.next = Some(new_node);
+        }
+    }
+
+    fn get_at(&self,n: usize) -> Option<RefNode<T>> {
+        let mut iter = self.iter();
+        iter.nth(n).map(|node| Rc::clone(&node))
+    }
+
+    fn remove_at(&mut self,n: usize) {
+        let size = self.size();
+
+        match n {
+            0 => self.remove_first(),
+            n if n < size => {
+                let before_target = self.get_at(n-1).unwrap();
+                let mut before_target = before_target.borrow_mut();
+                let target = before_target.next.as_ref().map(|ref_node| Rc::clone(ref_node)).unwrap();
+                let after_target = target.borrow_mut().next.take();
+                before_target.next = after_target
+            },
+            n if n == size => self.remove_last(),
+            _ => ()
         }
     }
 
@@ -146,6 +156,43 @@ mod tests {
     use super::*;
     use std::option::Option::Some;
 
+    #[test]
+    fn test_remove_at() {
+        let mut l = LinkedList::new();
+        l.insert_first(1);
+        l.insert_first(2);
+        l.insert_first(3);
+        l.remove_at(1);
+        assert_eq!(l.get_first().unwrap().borrow().data, 3);
+        assert_eq!(l.get_last().unwrap().borrow().data, 1);
+        assert_eq!(l.size(),2);
+        l.remove_at(1);
+        assert_eq!(l.size(),1);
+        assert_eq!(l.get_first().unwrap().borrow().data, 3);
+        assert_eq!(l.get_last().unwrap().borrow().data, 3);
+        l.remove_at(0);
+        assert_eq!(l.size(),0);
+    }
+
+    #[test]
+    fn test_get_at() {
+        let mut l = LinkedList::new();
+        l.insert_first(1);
+        l.insert_first(2);
+        l.insert_first(3);
+        assert_eq!(l.get_at(0).unwrap().borrow().data, 3);
+        assert_eq!(l.get_at(1).unwrap().borrow().data, 2);
+        assert_eq!(l.get_at(2).unwrap().borrow().data, 1);
+    }
+
+    #[test]
+    fn test_insert_last() {
+        let mut l = LinkedList::new();
+        l.insert_first(1);
+        l.insert_first(2);
+        l.insert_last(0);
+        assert_eq!(l.get_last().unwrap().borrow().data, 0);
+    }
 
     #[test]
     fn test_remove_last() {
@@ -154,10 +201,12 @@ mod tests {
         l.insert_first(2);
         l.insert_first(3);
         l.remove_last();
-        assert_eq!(l.get_last().unwrap().data,2);
+        assert_eq!(l.get_last().unwrap().borrow().data,2);
         assert_eq!(l.size(),2);
         l.remove_last();
+        assert_eq!(l.size(),1);
         l.remove_last();
+        assert_eq!(l.size(),0);
         l.remove_last();
         assert_eq!(l.size(),0);
     }
@@ -169,7 +218,7 @@ mod tests {
         l.insert_first(2);
         l.insert_first(3);
         l.remove_first();
-        assert_eq!(l.get_first().unwrap().data,2);
+        assert_eq!(l.get_first().unwrap().borrow().data,2);
         assert_eq!(l.size(),2);
     }
 
@@ -190,7 +239,7 @@ mod tests {
         l.insert_first(2);
         l.insert_first(3);
         let n = l.get_first().unwrap();
-        assert_eq!(n.data,3);
+        assert_eq!(n.borrow().data,3);
     }
 
     #[test]
@@ -200,7 +249,7 @@ mod tests {
         l.insert_first(2);
         l.insert_first(3);
         let n = l.get_last().unwrap();
-        assert_eq!(n.data,1);
+        assert_eq!(n.borrow().data,1);
     }
 
     #[test]
@@ -214,15 +263,6 @@ mod tests {
     #[test]
     fn test_node() {
         let n = Node::new(1,None);
-        let n = Node::new(2, Some(n));
-        let mut iter = n.iter();
-        assert_eq!(iter.next().unwrap().data,2);
-        assert_eq!(iter.next().unwrap().data,1);
-        assert!(iter.next().is_none());
-
-        let v = vec![1,2,3];
-        let n: Node<_> = v.into_iter().collect();
-        let v: Vec<_> = n.into_iter().collect();
-        assert_eq!(v, vec![1,2,3]);
+        let _ = Node::new(2, Some(n));
     }
 }
