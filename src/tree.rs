898a20bbb1d12a2e25838d2d05de5436a8a5bd2c
Vincent Herlemont <vincentherl@leszeros.com>
2021-02-05 17:06:08 +0000

    empty --

--- src/tree.rs
+++ src/tree.rs
@@ -4,7 +4,7 @@ use std::rc::Rc;
 
 type RefNode<T> = Rc<RefCell<Node<T>>>;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct Node<T> {
     data:T,
     children:Vec<RefNode<T>>
@@ -40,38 +40,53 @@ impl<T> From<RefNode<T>> for Tree<T> {
 impl<T> Tree<T> {
     fn depth_first_iter(&self) -> DepthFirstIter<T> {
         DepthFirstIter {
-            current: self.head.as_ref().map(|node| Rc::clone(node))
+            last: self.head.as_ref().map(|node| Rc::clone(node)),
+            next: None
         }
     }
 }
 
 struct DepthFirstIter<T> {
-    current: Option<RefNode<T>>
+    last: Option<RefNode<T>>,
+    next: Option<RefNode<T>>
 }
 
 impl<T> Iterator for DepthFirstIter<T> {
     type Item = RefNode<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
-
+        if let Some(last) = self.last {
+            if let Some(next) = self.next {
+                
+            }
+            None
+        }
+        None
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::ptr;
 
     #[test]
     fn test_tree(){
-        let n1 = Node::new(1);
+        let n1 = Node::new(2);
+        let n2 = Rc::clone(&n1);
+        let n3 = Node::new(2);
         let n2 = Node::new(2);
         n1.borrow_mut().push_children(n2);
         let t: Tree<_> = n1.into();
-        dbg!(&t);
-        let mut i = t.depth_first_iter();
-        {
-            let n = i.next();
-            dbg!(n);
-        }
+
+        // println!("{}", n1.as_ptr() == n2.as_ptr());
+        // println!("{}", n2.as_ptr() == n3.as_ptr());
+
+        // dbg!(&t);
+        // let mut i = t.depth_first_iter();
+        // {
+        //     let n = i.next();
+        //     dbg!(n);
+        // }
     }
 }
